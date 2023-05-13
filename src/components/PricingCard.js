0f1716965f5ba57c5026d4d41e144e285c4afb99
ssamarasam <samcsegct@gmail.com>
2023-05-13 23:10:53 +0530

    updated components

---
+++ src/components/PricingCard.js
@@ -0,0 +1,77 @@
+import React from "react";
+
+function PricingCard(props) {
+  return (
+    <div>
+      <div className="card mb-5 mb-lg-0">
+        <div className="card-body">
+          <h5 className="card-title text-muted text-uppercase text-center">
+            {props.data.plan}
+          </h5>
+          <h6 className="card-price text-center">
+            ${props.data.amount}
+            <span className="period">/month</span>
+          </h6>
+          <hr />
+          <ul className="fa-ul">
+            <li>
+              <span className="fa-li">
+                <i className="fas fa-check"></i>
+              </span>
+              {props.data.noOfUsers}
+            </li>
+            <li>
+              <span className="fa-li">
+                <i className="fas fa-check"></i>
+              </span>
+              {props.data.space}
+            </li>
+            <li>
+              <span className="fa-li">
+                <i className="fas fa-check"></i>
+              </span>
+              {props.data.noOfProject}
+            </li>
+            <li>
+              <span className="fa-li">
+                <i className="fas fa-check"></i>
+              </span>
+              {props.data.access}
+            </li>
+            <li className="text-muted">
+              <span className="fa-li">
+                <i className="fas fa-times"></i>
+              </span>
+              {props.data.privateProjects}
+            </li>
+            <li className="text-muted">
+              <span className="fa-li">
+                <i className="fas fa-times"></i>
+              </span>
+              {props.data.support}
+            </li>
+            <li className="text-muted">
+              <span className="fa-li">
+                <i className="fas fa-times"></i>
+              </span>
+              {props.data.subDomain}
+            </li>
+            <li className="text-muted">
+              <span className="fa-li">
+                <i className="fas fa-times"></i>
+              </span>
+              {props.data.reports}
+            </li>
+          </ul>
+          <div className="d-grid">
+            <a href="#" className="btn btn-primary text-uppercase">
+              Button
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PricingCard;
---
+++ src/components/PricingTable.js
@@ -0,0 +1,70 @@
+import React from "react";
+import PricingCard from "./PricingCard";
+
+function PricingTable() {
+  let data = [
+    {
+      plan: "Free",
+      amount: "0",
+      space: "5gb Storage",
+      noOfUsers: "Single User",
+      noOfProject: "Unlimited Public Projects",
+      access: "Community Access",
+      privateProjects: "Unlimited Private Projects",
+      support: "Dedicated Phone Support",
+      subDomain: "Free Subdomain",
+      reports: "Monthly Status Reports",
+    },
+    {
+      plan: "Plus",
+      amount: "9",
+      space: "50gb Storage",
+      noOfUsers: "5 Users",
+      noOfProject: "Unlimited Public Projects",
+      access: "Community Access",
+      privateProjects: "Unlimited Private Projects",
+      support: "Dedicated Phone Support",
+      subDomain: "Free Subdomain",
+      reports: "Monthly Status Reports",
+    },
+    {
+      plan: "Pro",
+      amount: "49",
+      space: "150gb Storage",
+      noOfUsers: "Unlimited Users",
+      noOfProject: "Unlimited Public Projects",
+      access: "Community Access",
+      privateProjects: "Unlimited Private Projects",
+      support: "Dedicated Phone Support",
+      subDomain: "Unlimited Free Subdomains",
+      reports: "Monthly Status Reports",
+    },
+  ];
+
+  return (
+    <div>
+      <section className="pricing py-5">
+        <div className="container">
+          <div className="row">
+            {data &&
+              data.map((card) => {
+                return (
+                  <div key={`pricing-details-$`} className="col-lg-4">
+                    <PricingCard data={card} />
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default PricingTable;
+
+{
+  /* <div className="col-lg-4">
+<PricingCard />
+</div> */
+}
